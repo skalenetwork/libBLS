8d3e5803d15c470e521a0dab048b093063d85cbb
Oleh Nikolaiev <oleg@skalelabs.com>
2019-04-12 15:47:57 +0300

    SKALE-890-add-licensing-headers

--- tools/verify_bls.cpp
+++ tools/verify_bls.cpp
@@ -1,26 +1,25 @@
-/**
- * @license
- * SKALE libBLS
- * Copyright (C) 2019-Present SKALE Labs
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
+/*
+    Copyright (C) 2018-2019 SKALE Labs
 
-/**
- * @file verify_bls.cpp
- * @date 2019
- */
+    This file is part of libBLS.
+
+    libBLS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libBLS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with skale-consensus.  If not, see <http://www.gnu.org/licenses/>.
+
+    @file verify_bls.cpp
+    @author Oleh Nikolaiev
+    @date 2019
+*/
 
 
 #include <fstream>
