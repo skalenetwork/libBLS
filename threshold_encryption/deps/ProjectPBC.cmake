707ffd47d214e6175fefd32b78c4ce688e93ca7c
Oleh Nikolaiev <oleg@skalelabs.com>
2019-06-04 16:17:43 +0300

    SKALE-811-implement-threshold-encryption

--- threshold_encryption/deps/ProjectPBC.cmake
+++ threshold_encryption/deps/ProjectPBC.cmake
@@ -9,7 +9,7 @@ set(PBC_INCLUDE_DIR "${PBC_PREFIX}/include/pbc")
 ExternalProject_Add( pbc
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/pbc-0.5.14
     BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ./configure --with-pic --enable-static --disable-shared --prefix=${PBC_PREFIX}
+    CONFIGURE_COMMAND libtoolize --force && aclocal && autoheader && automake --force-missing --add-missing && autoconf && ./configure --with-pic --enable-static --disable-shared --prefix=${PBC_PREFIX}
     BUILD_COMMAND make install
 )
 
