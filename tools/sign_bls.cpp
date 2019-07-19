0260c84fdd5115c2eb6056872394fbe08bf845d7
svetaro <sveta@skalelabs.com>
2019-07-19 17:34:42 +0300

    SKALE-1344 Fix of memory leak

--- tools/sign_bls.cpp
+++ tools/sign_bls.cpp
@@ -41,7 +41,8 @@ std::string ConvertToString(T field_elem) {
 
   field_elem.as_bigint().to_mpz(t);
 
-  char * tmp = mpz_get_str(NULL, 10, t);
+  char arr[mpz_sizeinbase (t, 10) + 2];
+  char * tmp = mpz_get_str(arr, 10, t);
   mpz_clear(t);
 
   std::string output = tmp;
