06d1a23e4f9dbcbd5eecbd5816281472a389ed5b
Oleh Nikolaiev <oleg@skalelabs.com>
2019-08-20 16:29:19 +0300

    SKALE-1443-fix-formating

--- bls/BLSutils.h
+++ bls/BLSutils.h
@@ -1,24 +1,24 @@
 /*
-  Copyright (C) 2018-2019 SKALE Labs
+Copyright (C) 2018-2019 SKALE Labs
 
-  This file is part of libBLS.
+This file is part of libBLS.
 
-  libBLS is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as published
-  by the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+libBLS is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  libBLS is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Affero General Public License for more details.
+libBLS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License
-  along with libBLS.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with libBLS.  If not, see <https://www.gnu.org/licenses/>.
 
-  @file BLSUtils.h
-  @author Sveta Rogova
-  @date 2019
+@file BLSUtils.h
+@author Sveta Rogova
+@date 2019
 */
 
 #include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
@@ -26,8 +26,7 @@
 #include <array>
 
 class BLSutils{
-
-    public:
+ public:
     template<class T>
     static std::string ConvertToString(T field_elem);
     static std::pair<libff::alt_bn128_Fq , libff::alt_bn128_Fq> ParseHint (std::string &);
@@ -38,17 +37,17 @@ class BLSutils{
 
 template<class T>
 std::string BLSutils::ConvertToString(T field_elem) {
-    mpz_t t;
-    mpz_init(t);
+  mpz_t t;
+  mpz_init(t);
 
-    field_elem.as_bigint().to_mpz(t);
+  field_elem.as_bigint().to_mpz(t);
 
-    char arr[mpz_sizeinbase (t, 10) + 2];
+  char arr[mpz_sizeinbase (t, 10) + 2];
 
-    char * tmp = mpz_get_str(arr, 10, t);
-    mpz_clear(t);
+  char * tmp = mpz_get_str(arr, 10, t);
+  mpz_clear(t);
 
-    std::string output = tmp;
+  std::string output = tmp;
 
-    return output;
+  return output;
 }
