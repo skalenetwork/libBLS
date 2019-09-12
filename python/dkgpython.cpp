19b6556dacd81f6573de4a5756b1d778de9bb2fd
Oleh Nikolaiev <oleg@skalelabs.com>
2019-09-12 18:12:44 +0300

    SKALE-1443-finish-dkg

--- python/dkgpython.cpp
+++ python/dkgpython.cpp
@@ -137,12 +137,12 @@ static PyObject* PyDkgObject_VerificationVector(struct PyDkgObject* self, PyObje
     PyObject* pSecondCoord = PyTuple_New(2);
     PyObject* pThirdCoord = PyTuple_New(2);
 
-    PyTuple_SET_ITEM(pFirstCoord,  0, MakePythonString(BLSutils::ConvertToString(ret_Val[i].X.c0).c_str()));
-    PyTuple_SET_ITEM(pFirstCoord,  1, MakePythonString(BLSutils::ConvertToString(ret_Val[i].X.c1).c_str()));
-    PyTuple_SET_ITEM(pSecondCoord, 0, MakePythonString(BLSutils::ConvertToString(ret_Val[i].Y.c0).c_str()));
-    PyTuple_SET_ITEM(pSecondCoord, 1, MakePythonString(BLSutils::ConvertToString(ret_Val[i].Y.c1).c_str()));
-    PyTuple_SET_ITEM(pThirdCoord,  0, MakePythonString(BLSutils::ConvertToString(ret_Val[i].Z.c0).c_str()));
-    PyTuple_SET_ITEM(pThirdCoord,  1, MakePythonString(BLSutils::ConvertToString(ret_Val[i].Z.c1).c_str()));
+    PyTuple_SET_ITEM(pFirstCoord,  0, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].X.c0).c_str()));
+    PyTuple_SET_ITEM(pFirstCoord,  1, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].X.c1).c_str()));
+    PyTuple_SET_ITEM(pSecondCoord, 0, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].Y.c0).c_str()));
+    PyTuple_SET_ITEM(pSecondCoord, 1, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].Y.c1).c_str()));
+    PyTuple_SET_ITEM(pThirdCoord,  0, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].Z.c0).c_str()));
+    PyTuple_SET_ITEM(pThirdCoord,  1, MakePythonString(BLSutils::ConvertToString<libff::alt_bn128_Fq>(ret_Val[i].Z.c1).c_str()));
 
     PyObject* pyPublicKey = PyList_New(3);
     PyList_SetItem(pyPublicKey, 0, pFirstCoord);
@@ -279,9 +279,15 @@ static PyObject* PyDkgObject_GetPublicKeyFromSecretKey(struct PyDkgObject* self,
     return -1;
   }
 
+  std::cout << 1 << '\n';
+
   libff::alt_bn128_Fr secret_key = libff::alt_bn128_Fr(incoming_data.c_str());
 
+  std::cout << 2 << '\n';
+
   libff::alt_bn128_G2 public_key = self->pDKG->GetPublicKeyFromSecretKey(secret_key);
+
+  std::cout << 3 << '\n';
 
   PyObject* pFirstCoord = PyTuple_New(2);
   PyObject* pSecondCoord = PyTuple_New(2);
