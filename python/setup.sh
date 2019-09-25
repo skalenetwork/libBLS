3ec1e440e2a896e2476c12bc1789d78564f85035
Oleh Nikolaiev <oleg@skalelabs.com>
2019-09-25 18:50:25 +0300

    SKALE-1545-fix-travis

--- python/setup.sh
+++ python/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 setup.py build
+python3.6 setup.py build
 # or even simply run: setup.py build
 echo ================ module built =============
 ldd ./build/lib.linux-x86_64-3.6/dkgpython.cpython-36m-x86_64-linux-gnu.so
