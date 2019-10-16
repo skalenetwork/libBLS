48d7c47be42b1733dfe2ec3ab7a904d686f49d9b
Oleh Nikolaiev <oleg@skalelabs.com>
2019-10-16 22:24:02 +0300

    SKALE-1565-fix-python-module

--- python/setup.py
+++ python/setup.py
@@ -11,11 +11,14 @@ os.environ["LD" ] = "ld" + strCompilerCommonFlagsSuffix
 
 dkgpython_module = Extension('dkgpython',
                              sources = ['dkgpython.cpp'],
-                             include_dirs = ['..', '../bls', '../dkg', '../third_party',
-                             '../libff', '../mpir'],
-                             library_dirs = ['../build', '../deps/lib', '../build/deps/lib',
-                             '../build/libff/libff'],
-                             libraries = ['bls', 'ff', 'gmpxx', 'gmp']
+                             include_dirs = ['..', '../bls', '../dkg', '../third_party', '../deps/',
+                             '../deps/deps_inst/x86_or_x64/include',
+                             '../deps/deps_inst/x86_or_x64/include/libff'],
+
+                             library_dirs = ['../build', '../deps/deps_inst/x86_or_x64/lib',
+                             '../deps/deps_inst/x86_or_x64/lib/libff'],
+
+                             libraries = ['bls', 'ff', 'gmpxx', 'gmp', 'procps']
                              )
 
 setup(name = 'dkgpython',
