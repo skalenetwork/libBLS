5014f6787591de742a8c8663c30ac66ca3b68f03
Sergiy Lavrynenko <sergiy@skalelabs.com>
2020-04-29 10:54:55 +0300

    SKALE-2476 build script improvement

--- deps/build.sh
+++ deps/build.sh
@@ -99,7 +99,7 @@ simple_find_tool_program () { # program_name, var_name_to_export_full_path, is_o
 	#TMP_CMD="export \$\"$2\"=$(which \"$1\")"
 	TMP_R1=$2
 	TMP_W1=$(which "$1")
-	TMP_CMD="export \"$TMP_R1\"=\"$TMP_W1\""
+	TMP_CMD="export $TMP_R1=\"$TMP_W1\""
 	#
 	$TMP_CMD
 	TMP_CMD="echo ${!2}"
