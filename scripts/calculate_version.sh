243412cf97254de6008b656f018fb84ab02dd43c
Oleh Nikolaiev <oleg@skalelabs.com>
2020-08-18 11:24:42 +0300

    fix codacy]

--- scripts/calculate_version.sh
+++ scripts/calculate_version.sh
@@ -6,17 +6,17 @@ USAGE_MSG='Usage: BRANCH=[BRANCH] calculate_version.sh'
 if [ -z "$BRANCH" ]
 then
     (>&2 echo 'You should provide branch')
-    echo $USAGE_MSG
+    echo "$USAGE_MSG"
     exit 1
 fi
 
 
-if [ -z $VERSION ]; then
+if [ -z "$VERSION" ]; then
       echo "The base version is not set."
       exit 1
 fi
 
-if [[ $BRANCH == 'master' ]]; then
+if [[ "$BRANCH" == 'master' ]]; then
     echo $VERSION
     exit 1
 fi
@@ -26,7 +26,7 @@ git fetch --tags > /dev/null
 for (( NUMBER=0; ; NUMBER++ ))
 do
     FULL_VERSION="$VERSION-$BRANCH.$NUMBER"
-    if ! [[ $(git tag -l | grep $FULL_VERSION) ]]; then
+    if ! [[ $(git tag -l | grep "$FULL_VERSION") ]]; then
         echo "$FULL_VERSION" | tr / -
         break
     fi
