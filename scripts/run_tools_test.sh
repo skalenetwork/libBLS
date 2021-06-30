ce4fe5f04386f3f7ad5acbe02e974b52742543db
Oleh <oleg@skalelabs.com>
2021-06-30 17:37:49 -0500

    SKALE-4161 fix codacy

--- scripts/run_tools_test.sh
+++ scripts/run_tools_test.sh
@@ -3,7 +3,7 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_DIR="$(dirname $DIR)"
+PROJECT_DIR="$(dirname "$DIR")"
 WORK_DIR="$PROJECT_DIR"/build
 echo "$WORK_DIR"
 cd "$WORK_DIR"
