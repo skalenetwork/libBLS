35293405bb0433564c7d6046f4962ebe3f2ecec6
Oleh <oleg@skalelabs.com>
2021-06-30 17:30:09 -0500

    SKALE-4161 fix codacy

--- scripts/calculate_version.sh
+++ scripts/calculate_version.sh
@@ -3,13 +3,13 @@
 BRANCH=$1
 VERSION=$2
 
-if [ -z $BRANCH ]
+if [ -z "$BRANCH" ]
 then
       echo "A branch is not set."
       exit 1
 fi
 
-if [ -z $VERSION ]
+if [ -z "$VERSION" ]
 then
       echo "The base version is not set."
       exit 1
@@ -17,17 +17,17 @@ fi
 
 git fetch --tags
 
-if [ $BRANCH = "master" ]
+if [ "$BRANCH" = "master" ]
 then
-    echo $VERSION
+    echo "$VERSION"
     exit 0
 fi
 
 LABEL="develop"
-if [ $BRANCH = "stable" ]
+if [ "$BRANCH" = "stable" ]
 then
     LABEL="stable"
-elif [ $BRANCH = "beta" ]
+elif [ "$BRANCH" = "beta" ]
 then
     LABEL="beta"
 fi
@@ -35,7 +35,7 @@ fi
 for (( VERSION_NUMBER=0; ; VERSION_NUMBER++ ))
 do
     RESULT_VERSION="$VERSION-$LABEL.$VERSION_NUMBER"
-    if ! [[ $(git tag -l | grep $RESULT_VERSION) ]]; then
+    if ! [[ $(git tag -l | grep "$RESULT_VERSION") ]]; then
         echo "$RESULT_VERSION" | tr / -
         break
     fi
--- scripts/run_tools_test.sh
+++ scripts/run_tools_test.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_DIR=$(dirname $DIR)
-WORK_DIR=$PROJECT_DIR/build
-echo $WORK_DIR
-cd $WORK_DIR
+PROJECT_DIR="$(dirname $DIR)"
+WORK_DIR="$PROJECT_DIR"/build
+echo "$WORK_DIR"
+cd "$WORK_DIR"
 
 DATA_FILE=data.txt
 SIGNATURE_FILE=signature.txt
@@ -12,11 +14,11 @@ RANDOM_HASH_STR="9ebe2b024baa072bb02f1df851a88900"
 
 calculate_hash() {
     HASH_FILE=hash.json
-    touch $HASH_FILE
+    touch "$HASH_FILE"
     JSON_STRING=$( jq -n \
-                  --arg msg $RANDOM_HASH_STR \
+                  --arg msg "$RANDOM_HASH_STR" \
                   '{message: $msg}' )
-    echo $JSON_STRING > $HASH_FILE
+    echo "$JSON_STRING" > "$HASH_FILE"
     ./hash_g1 --t 11 --n 16
 }
 
@@ -25,23 +27,23 @@ run_tools_test_with_individual_keys() {
     for i in {0..15}
     do
         individual_dkg_keys="$individual_dkg_keys --input data_for_$i-th_participant.json"
-        ./dkg_keygen --t 11 --n 16 --j $i
+        ./dkg_keygen --t 11 --n 16 --j "$i"
     done
     echo "Generated individual dkg keys"
-    ./dkg_glue --t 11 --n 16 $individual_dkg_keys
+    ./dkg_glue --t 11 --n 16 "$individual_dkg_keys"
     echo "Generated individual bls keys"
     
     calculate_hash
     echo "Calculated hash"
 
-    touch $DATA_FILE
-    echo $RANDOM_HASH_STR > $DATA_FILE
+    touch "$DATA_FILE"
+    echo "$RANDOM_HASH_STR" > "$DATA_FILE"
 
     for i in {0..15}
     do
         SIGNATURE="signature"$i".json"
-        ./sign_bls --t 11 --n 16 --j $i --key "BLS_keys" --input $DATA_FILE --output $SIGNATURE
-        ./verify_bls --t 11 --n 16 --j $i --input $SIGNATURE
+        ./sign_bls --t 11 --n 16 --j "$i" --key "BLS_keys" --input "$DATA_FILE" --output "$SIGNATURE"
+        ./verify_bls --t 11 --n 16 --j "$i" --input "$SIGNATURE"
     done
     echo "Completed individual signatures and verified them"
     
@@ -51,10 +53,10 @@ run_tools_test_with_individual_keys() {
         SIGNATURE="signature"$i".json"
         individual_signatures="$individual_signatures --input $SIGNATURE"
     done
-    ./bls_glue --t 11 --n 16 $individual_signatures --output $SIGNATURE_FILE
+    ./bls_glue --t 11 --n 16 "$individual_signatures" --output "$SIGNATURE_FILE"
     echo "Generated common bls signature"
     
-    ./verify_bls --t 11 --n 16 --input $SIGNATURE_FILE
+    ./verify_bls --t 11 --n 16 --input "$SIGNATURE_FILE"
     echo "Verified common bls signature"
 }
 
@@ -65,13 +67,13 @@ run_tools_test_with_common_keys() {
     calculate_hash
     echo "Calculated hash"
 
-    touch $DATA_FILE
-    echo $RANDOM_HASH_STR > $DATA_FILE
+    touch "$DATA_FILE"
+    echo "$RANDOM_HASH_STR" > "$DATA_FILE"
 
-    ./sign_bls --t 11 --n 16 --key BLS_keys --input $DATA_FILE --output $SIGNATURE_FILE
+    ./sign_bls --t 11 --n 16 --key BLS_keys --input "$DATA_FILE" --output "$SIGNATURE_FILE"
     echo "Generated common bls signature"
 
-    ./verify_bls --t 11 --n 16 --input $SIGNATURE_FILE
+    ./verify_bls --t 11 --n 16 --input "$SIGNATURE_FILE"
     echo "Verified common bls signature"
 
     ./generate_key_system --t 11 --n 16 --output output.json
@@ -83,7 +85,7 @@ clean() {
     rm hash.json g1.json
     rm common_public_key.json
     rm signature*.json
-    rm $SIGNATURE_FILE $DATA_FILE
+    rm "$SIGNATURE_FILE" "$DATA_FILE"
 }
 
 run_tools_test_with_individual_keys
