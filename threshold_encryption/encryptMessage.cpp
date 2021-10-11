074b69b389a8d6d5fe11086bf0a91dabe357dc94
Oleh Nikolaiev <oleg@skalelabs.com>
2021-10-11 18:16:00 +0300

    SKALE-4510 format code

--- threshold_encryption/encryptMessage.cpp
+++ threshold_encryption/encryptMessage.cpp
@@ -19,10 +19,10 @@
 //     return 0;
 // }
 extern "C" {
-const char* encryptMessage(const char* data, const char* key) {
+const char* encryptMessage( const char* data, const char* key ) {
     crypto::ThresholdUtils::initCurve();
     auto ciphertext_string = crypto::TE::encryptMessage( data, key );
 
-    return std::move(ciphertext_string.c_str());
+    return std::move( ciphertext_string.c_str() );
 }
 }
