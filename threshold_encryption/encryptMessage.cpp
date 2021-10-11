2fb943c6b509f041506e4b9508b4f4e71f280264
Oleh Nikolaiev <oleg@skalelabs.com>
2021-10-11 20:01:48 +0300

    SKALE-4510 format code

--- threshold_encryption/encryptMessage.cpp
+++ threshold_encryption/encryptMessage.cpp
@@ -1,28 +1,13 @@
 #include <threshold_encryption.h>
 #include <tools/utils.h>
-// #include <iostream>
 
-// int main( int argc, char* argv[] ) {
-//     if ( argc != 3 ) {
-//         // argv[0] is path to .js file
-//         std::cout << "Wrong number of arguments\n" << argc << '\n';
-//         return 1;
-//     }
-
-//     std::string message = argv[1];
-//     std::string common_public_str = argv[2];
-
-//     crypto::ThresholdUtils::initCurve();
-//     auto ciphertext_string = crypto::TE::encryptMessage( message, common_public_str );
-
-//     std::cout << "cipher " << ciphertext_string;
-//     return 0;
-// }
 extern "C" {
+
 const char* encryptMessage( const char* data, const char* key ) {
     crypto::ThresholdUtils::initCurve();
     auto ciphertext_string = crypto::TE::encryptMessage( data, key );
 
     return std::move( ciphertext_string.c_str() );
 }
+
 }
