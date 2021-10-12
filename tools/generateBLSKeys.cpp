3c81934068bed116a6ff7d8a60f9e348f6dc1f8d
Oleh Nikolaiev <oleg@skalelabs.com>
2021-10-12 14:45:39 +0300

    SKALE-4510 format codee

--- tools/generateBLSKeys.cpp
+++ tools/generateBLSKeys.cpp
@@ -1,7 +1,7 @@
 #include <threshold_encryption.h>
 #include <tools/utils.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 int main() {
     crypto::ThresholdUtils::initCurve();
@@ -9,25 +9,25 @@ int main() {
     libff::alt_bn128_G2 public_key = secret_key * libff::alt_bn128_G2::one();
 
     std::ofstream secretKeyFile;
-    secretKeyFile.open("secret_key.txt");
-    secretKeyFile << crypto::ThresholdUtils::fieldElementToString(secret_key);
-    
-    auto vector_coordinates = crypto::ThresholdUtils::G2ToString(public_key);
-    
+    secretKeyFile.open( "secret_key.txt" );
+    secretKeyFile << crypto::ThresholdUtils::fieldElementToString( secret_key );
+
+    auto vector_coordinates = crypto::ThresholdUtils::G2ToString( public_key );
+
     std::string result = "";
-    for (const auto& coord: vector_coordinates) {
+    for ( const auto& coord : vector_coordinates ) {
         result += coord;
     }
 
     std::ofstream publicKeyFile;
-    publicKeyFile.open("bls_public_key.txt");
+    publicKeyFile.open( "bls_public_key.txt" );
     publicKeyFile << result;
 
     libff::alt_bn128_Fr message_number = libff::alt_bn128_Fr::random_element();
-    std::string message = crypto::ThresholdUtils::fieldElementToString(message_number, 16);
+    std::string message = crypto::ThresholdUtils::fieldElementToString( message_number, 16 );
 
     std::ofstream messageFile;
-    messageFile.open("message.txt");
+    messageFile.open( "message.txt" );
     messageFile << message;
 
     return 0;
