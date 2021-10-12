cdfb8adbda0e66e5e261c015fc1943c98e0dfdb4
Oleh Nikolaiev <oleg@skalelabs.com>
2021-10-12 13:46:13 +0300

    SKALE-4510 add tests

---
+++ test/generateBLSKeys.cpp
@@ -0,0 +1,34 @@
+#include <threshold_encryption/threshold_encryption.h>
+#include <tools/utils.h>
+#include <iostream>
+#include <fstream>
+
+int main() {
+    crypto::ThresholdUtils::initCurve();
+    libff::alt_bn128_Fr secret_key = libff::alt_bn128_Fr::random_element();
+    libff::alt_bn128_G2 public_key = secret_key * libff::alt_bn128_G2::one();
+
+    std::ofstream secretKeyFile;
+    secretKeyFile.open("secret_key.txt");
+    secretKeyFile << crypto::ThresholdUtils::fieldElementToString(secret_key);
+    
+    auto vector_coordinates = crypto::ThresholdUtils::G2ToString(public_key);
+    
+    std::string result = "";
+    for (const auto& coord: vector_coordinates) {
+        result += coord;
+    }
+
+    std::ofstream publicKeyFile;
+    publicKeyFile.open("bls_public_key.txt");
+    publicKeyFile << result;
+
+    libff::alt_bn128_Fr message_number = libff::alt_bn128_Fr::random_element();
+    std::string message = crypto::ThresholdUtils::fieldElementToString(message_number, 16);
+
+    std::ofstream messageFile;
+    messageFile.open("message.txt");
+    messageFile << message;
+
+    return 0;
+}
