3dc478d1824ae068f1de069f053e6cbc64068e98
Oleh Nikolaiev <oleg@skalelabs.com>
2025-01-09 18:15:13 +0000

    1900 format

--- deps/build.sh
+++ deps/build.sh
@@ -585,13 +585,12 @@ then
 		if [[ "${WITH_EMSCRIPTEN}" -eq 1 ]];
 		then
 			BOOST_LIBRARIES="program_options"
-                else
+		else
 			BOOST_LIBRARIES="system,thread,filesystem,regex,atomic,program_options"
-                        if [ "$SKALED_DEPS_CHAIN" = "1" ];
-                        then
-                                BOOST_LIBRARIES="${BOOST_LIBRARIES},context,iostreams,fiber,log,chrono"
-                        fi
-
+			if [ "$SKALED_DEPS_CHAIN" = "1" ];
+			then
+					BOOST_LIBRARIES="${BOOST_LIBRARIES},context,iostreams,fiber,log,chrono"
+			fi
 		fi
 		eval ./bootstrap.sh --prefix="$INSTALL_ROOT" --with-libraries="$BOOST_LIBRARIES"
 
@@ -603,23 +602,23 @@ then
 
 		if [ ${ARCH} = "arm" ]
 		then
-				sed -i -e 's#using gcc ;#using gcc : arm : /usr/local/toolchains/gcc7.2-arm/bin/arm-linux-gnueabihf-g++ ;#g' project-config.jam
-				eval ./b2 "${CONF_CROSSCOMPILING_OPTS_BOOST}" cxxflags=-fPIC cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
+			sed -i -e 's#using gcc ;#using gcc : arm : /usr/local/toolchains/gcc7.2-arm/bin/arm-linux-gnueabihf-g++ ;#g' project-config.jam
+			eval ./b2 "${CONF_CROSSCOMPILING_OPTS_BOOST}" cxxflags=-fPIC cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
 		else
-				if [ "$UNIX_SYSTEM_NAME" = "Darwin" ];
+			if [ "$UNIX_SYSTEM_NAME" = "Darwin" ];
+			then
+				eval ./b2 cxxflags=-fPIC toolset=clang cxxstd=17 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
+			else
+				if [[ "${WITH_EMSCRIPTEN}" -eq 1 ]];
 				then
-						eval ./b2 cxxflags=-fPIC toolset=clang cxxstd=17 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
+					eval ./b2 toolset=emscripten cxxflags=-fPIC cxxstd=14 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --disable-icu --layout=system variant=${variant} link=static install
+					cd bin.v2/libs/program_options/build/emscripten-2.0.31/${variant}/cxxstd-14-iso/link-static/threading-multi/
+					eval emar q "libboost_program_options.a" ./*.bc
+					eval cp "libboost_program_options.a" "${LIBRARIES_ROOT}"
 				else
-						if [[ "${WITH_EMSCRIPTEN}" -eq 1 ]];
-						then
-								eval ./b2 toolset=emscripten cxxflags=-fPIC cxxstd=14 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --disable-icu --layout=system variant=${variant} link=static install
-								cd bin.v2/libs/program_options/build/emscripten-2.0.31/${variant}/cxxstd-14-iso/link-static/threading-multi/
-								eval emar q "libboost_program_options.a" ./*.bc
-								eval cp "libboost_program_options.a" "${LIBRARIES_ROOT}"
-						else
-								eval ./b2 cxxflags=-fPIC cxxstd=14 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
-						fi
+					eval ./b2 cxxflags=-fPIC cxxstd=14 cflags=-fPIC "${PARALLEL_MAKE_OPTIONS}" --prefix="$INSTALL_ROOT" --layout=system variant=${variant} link=static threading=multi install
 				fi
+			fi
 		fi
 		cd ..
 		cd "$SOURCES_ROOT"
