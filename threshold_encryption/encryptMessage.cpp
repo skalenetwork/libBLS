255180572d7faa1fd476cbdab7f11e6c873d65eb
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2025-03-18 19:40:24 +0000

    small emscripten fix

--- threshold_encryption/encryptMessage.cpp
+++ threshold_encryption/encryptMessage.cpp
@@ -30,6 +30,6 @@ const char* encryptMessage( const char* data, const char* key ) {
     libBLS::ThresholdUtils::initCurve();
     auto ciphertext_string = libBLS::TE::encryptMessage( data, key );
 
-    return std::move( ciphertext_string.c_str() );
+    return std::move( ciphertext_string.first.c_str() );
 }
 }
