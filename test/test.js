b31433060ea8be7e916f59413a262218c0d03f8e
Oleh Nikolaiev <oleg@skalelabs.com>
2025-03-19 12:20:42 +0000

    #225 update tests

--- test/test.js
+++ test/test.js
@@ -3,9 +3,6 @@ const ModuleFactory = require("./encrypt.js");
 const BLS_PUBLIC_KEY = process.argv[2];
 const TX_DATA = process.argv[3];
 
-// const BLS_PUBLIC_KEY = "01794f875120054796826a296e528582adba4cf96729a97310d68fcd818b44280970fa00673e46c9399c8c00a5076061a3f269afee887b2653ce261ad249ab4d2c5544e7a0ac777e6566b9ef969c8f0f7f092e1f8fff463b957d5030c6cc77f4298d9e76f54fdf955a9879532f3189691f6a9aceee3b234f14be170c1a2d2c15";
-// const TX_DATA = "109b07b3342ab2f561c22a69d9b668db552692321820ceed2e907861c72cf77f";
-
 ModuleFactory().then((Module) => {
     // Use the Module object after it is initialized
     const result = Module.ccall(
