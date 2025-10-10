aa1e0b26cc11520493c3b877e42a3475a8ba7e22
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2025-10-10 17:39:28 +0100

    #265 fix build error

--- threshold_encryption/ThresholdEncryption.h
+++ threshold_encryption/ThresholdEncryption.h
@@ -180,7 +180,7 @@ public:
     static void validateCombinedDecryption(
         const Ciphertext& _ciphertext, const AES256Key& _aesKey, const TEPublicKey& _publicKey );
 
-    static void validateCombinedDecryptionBatch( const std::vector< Ciphertext >& _ciphertexts,
+    static std::vector< bool > validateCombinedDecryptionBatch( const std::vector< Ciphertext >& _ciphertexts,
         const std::vector< AES256Key >& _aesKeys, const TEPublicKey& _publicKey );
 
     /**
