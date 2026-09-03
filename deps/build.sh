724afca445de802a1dc7e44c389f630659976256
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-09-03 17:04:31 +0100

    migrate to ubuntu 24.04

--- deps/build.sh
+++ deps/build.sh
@@ -212,7 +212,6 @@ CXX_STANDARD=20
 export CFLAGS="$CFLAGS -fPIC"
 export CXXFLAGS="$CXXFLAGS -fPIC"
 WITH_OPENSSL="yes"
-OPENSSL_GIT_REF="${OPENSSL_GIT_REF:-OpenSSL_1_1_1n}"
 
 WITH_BOOST="yes"
 WITH_ZLIB="yes"
@@ -719,7 +718,7 @@ then
 			echo -e "${COLOR_INFO}configuring it${COLOR_DOTS}...${COLOR_RESET}"
 			cd openssl
 			eval git fetch
-			git checkout --detach "$OPENSSL_GIT_REF"
+			git checkout --detach openssl-3.0.22
 			if [[ "$ARCH" = "x86_or_x64" || "$ARCH" = "wasm32-emscripten" ]];
 			then
 				if [ "$UNIX_SYSTEM_NAME" = "Darwin" ];
